3e441bca1310fee36ffac6b39cdcb6cbaf893861
anayensi <vnvyensi@gmail.com>
2021-01-22 13:27:04 -0500

    commit tree e51069bda036ed2446857aa06639bb8453119bb9

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,19 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    def parse
+        puts "parses CVS emails"
+    end
+    def parse
+        puts "parses space delimited emails"
+    end
+    def parse
+        puts "parses both CSV and space delimited emails"
+    end
+    def parse
+        puts 'parses and removes duplicate emails'
+    end
+end
+
+emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
